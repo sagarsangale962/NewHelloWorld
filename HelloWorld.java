7c1bbc3615aff90c56e26bc229668db7d03c4785
Sagar Sangale <103577188+sagarsangale962@users.noreply.github.com>
2023-03-14 12:53:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello. This is written in java. ********Helllo*****");
+    System.out.println("Hello. This is written in java. ********Helllo******");
     System.out.println("******This is second change. FInally java code run by sagar*******");
   }
 }
